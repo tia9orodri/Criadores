7210627eb700e8692db1a30ccc5318be37f76141
Tiago Rodrigues <aluno19058@ipt.pt>
2021-03-22 16:10:12 +0000

    Criadores e fotografias - completo

--- Criadores/Criadores/Models/Criadores.cs
+++ Criadores/Criadores/Models/Criadores.cs
@@ -4,6 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Criadores.Models {
+    /// <summary>
+    /// Identifica um criador 
+    /// </summary>
     public class Criadores {
+
+        /// <summary>
+        /// Identificador único de um criador
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do Criador
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Nome comercial do criador
+        /// </summary>
+        public string NomeComercial { get; set; }
+
+        /// <summary>
+        /// Morada do criador
+        /// </summary>
+        public string Morada { get; set; }
+
+        /// <summary>
+        /// Codigo Postal do criador
+        /// </summary>
+        public int CodigoPostal { get; set; }
+
+        /// <summary>
+        /// Numero de telemovel do criador
+        /// </summary>
+        public int Telemovel { get; set; }
+
+        /// <summary>
+        /// Email do Criador
+        /// </summary>
+        public string Email { get; set; }
     }
 }
--- Criadores/Criadores/Models/Fotografias.cs
+++ Criadores/Criadores/Models/Fotografias.cs
@@ -4,6 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Criadores.Models {
+    /// <summary>
+    /// Lista de fotografias associadas a um cão
+    /// </summary>
     public class Fotografias {
+
+        /// <summary>
+        /// Identificador unico da fotografia
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Fotografia de um cão
+        /// </summary>
+        public string Fotografia { get; set; }
+
+        /// <summary>
+        /// Data em que foto foi tirada
+        /// </summary>
+        public DateTime DataFoto { get; set; }
+
+        /// <summary>
+        /// Local onde foto foi tirada
+        /// </summary>
+        public string LocalFoto { get; set; }
     }
 }
