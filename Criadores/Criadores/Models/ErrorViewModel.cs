731f9769dc7659c1e6feeeea9ac107cd2e814d91
Tiago Rodrigues <aluno19058@ipt.pt>
2021-03-22 15:20:21 +0000

    Inicial

---
+++ Criadores/Criadores/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Criadores.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
